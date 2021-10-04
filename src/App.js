d2f896c81e1ff00a361183624a79305dd2e35f16
rach <sbadui@gmail.com>
2021-10-04 16:08:16 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+
+import xlsxFile from './products.csv';
+import { useState, useEffect  } from 'react';
 import './App.css';
+import Papa from "papaparse";
 
 function App() {
+  const [searchKey, setSearchKey] = useState();
+  const [results, setResults] = useState();
+
+
+
+const searchOptions= (e)=>{
+  setSearchKey(e.target.value)
+  console.log("setSearchKey",e.target.value);
+}
+
+const submitButton= ()=>{console.log("submitted",searchKey);
+  Papa.parse(xlsxFile, {
+    download: true,
+    complete: function(papaResults) {
+      const db=papaResults.data;
+      db.map((x,i)=>{
+        var productTitle=db[i][0]
+        if (productTitle.search(searchKey)>=0) { //if the key that was search appears in the title
+          console.log(i,"mpapaResults",db[i][0]);
+        }
+      
+      })
+     
+    }
+  });
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="searchBar">
+        <h2>I am looking for:</h2>
+        <input type="text" onChange={searchOptions} />
+        <button type="submit" onClick={submitButton}>
+          Search
+        </button>
+      </div>
+      <div className="showResults">
+
+      </div>
+     
+
+
+
     </div>
   );
 }
